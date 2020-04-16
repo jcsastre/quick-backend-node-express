6b9fc1819c4e6c1ca78612ef695dd170d087d5c6
Juan Carlos Sastre <juan.carlos.sastre.iglesias@gmail.com>
2020-04-16 17:37:17 +0200

    Places and users controllers.

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const usersRoutes = require('./routes/users-routes');
 const placesRoutes = require('./routes/places-routes');
+const HttpError = require('./models/http-error');
 
 const app = express();
 
+app.use(bodyParser.json());
+
+app.use('/api/users', usersRoutes); // => /api/users/...
 app.use('/api/places', placesRoutes); // => /api/places/...
+
+app.use((req, res, next) => {
+    const error = new HttpError('Could not find this route.', 404);
+    throw error;
+});
 
 app.use((error, req, res, next) => {
     if (res.headerSent) {
